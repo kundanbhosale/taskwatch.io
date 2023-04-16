4c8052a487b518704dd3804a6d3dd763904c458a
Kundan Bhosale <kundanbhosale99@gmail.com>
2023-04-16 20:56:44 +0530

    added tracking and ga

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { registerSW } from 'virtual:pwa-register'
+import * as Sentry from '@sentry/react'
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = import.meta.env.VITE_GA_MESUREMENT_ID // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID)
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 // add this to prompt for a refresh
 const updateSW = registerSW({
